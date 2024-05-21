89d33f1f2b53f64a409ed42e533df687883f18ae
MSG <muhammadsahalg@gmail.com>
2024-05-21 13:12:29 +0200

    Basic navigation creation

--- src/surveillance_bot/scripts/main.py
+++ src/surveillance_bot/scripts/main.py
@@ -2,49 +2,79 @@
 import rospy
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
+from pid import PIDController
+from navigator import Navigator
+import numpy as np
 
 def init_node():
     # Create drone node
-    rospy.init_node('drone', anonymous=True)
+    rospy.init_node('surveillance_bot', anonymous=True)
 
-def takeoff():
-    takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=1)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        #Ensure that node has been subscribed to by takeoff
-        is_connected = takeoff_pub.get_num_connections() > 0
-        if is_connected:
-            takeoff_pub.publish()
-            rospy.loginfo('Took off')
-            break
-        rate.sleep()
+def parse_coordinates(coords):
+    return list(map(float, coords.split(',')))
 
-def move():
-    move_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+def navigateTo(target=None):
+
+    # -- Load in Map
+    # Map start, target to image coords
+    # Build PRM on that map
+    # Generate path from PRM and convert to world points
+
+    navigator = Navigator()
+
+    start = None # Starting position
+    goal = None # Ending position
+
+    waypoints = [np.array([5,5])]
+    waypoint_count = 0
     
-    vel_msg = Twist()
-    vel_msg.linear.x = 1 # Move forward
 
-    while not rospy.is_shutdown():
-        #Ensure that node has been subscribed to by takeoff
-        is_connected = move_pub.get_num_connections() > 0
-        if is_connected:
-            move_pub.publish(vel_msg)
-            rospy.loginfo('Flying in the air')
-            break
-    rospy.sleep(rospy.Duration(3.0)) # Fly for 3 seconds
+    pid_linear = PIDController(1,1,1)
+    pid_angular = PIDController(1,1,1)
+
+
+    # Follow PID Iteratively
+    while (not rospy.is_shutdown() and waypoint_count < len(waypoints)):
+
+        # Get the current position and angular velocity in world
+        position, yaw = navigator.get_model_state()
+        position = np.array([position.x, position.y])
+
+        # Compute target orientation based on current waypoint
+        direction = waypoints[waypoint_count] - position
+        target_angular = np.arctan2(direction[1], direction[0])
+
+        # Set the goals
+        pid_linear.goal = waypoints[waypoint_count]
+        pid_angular.goal = target_angular
+
+        # Compute PID updates for linear and angular velocities
+        update_linear = pid_linear.getUpdate(position)
+        update_angular = pid_angular.getUpdate(yaw)
+
+        # Publish velocities
+        navigator.publish_velocity(update_linear, update_angular)
+
+        # Reached current waypoint, advance next
+        if np.linalg.norm(position - waypoints[waypoint_count]) < 0.05:
+            waypoint_count += 1
+            if waypoint_count < len(waypoints):
+                update_linear.goal = waypoints[waypoint_count]
     
-    vel_msg.linear.x = 0 # Halt the drone
-    move_pub.publish(vel_msg) 
-    rospy.sleep(rospy.Duration(1)) # Allow time to halt before landing
+    rospy.loginfo("Successfully arrived at location: %s ! ", position)
+
+    stop_vel = np.zeros(3)
 
 
-
-
-    
 
 if __name__ == '__main__':
     try:
         init_node() # Create node
+        try:
+            target = parse_coordinates(input())
+            navigateTo()
+        except:
+            print("Please specify the target location in the form x,y")
+        
     except rospy.ROSInterruptException:
         pass
--- src/surveillance_bot/scripts/pid.py
+++ src/surveillance_bot/scripts/pid.py
@@ -8,8 +8,8 @@ from geometry_msgs.msg import Twist
 from gazebo_msgs.srv import GetModelState
 
 class PIDController:
-    def __init__(self, goal, kp, ki, kd):
-        self.goal = goal
+    def __init__(self, kp, ki, kd):
+        
 
         self.kp = kp
         self.ki = ki
@@ -18,6 +18,9 @@ class PIDController:
         self.prev_time = 0
         self.prev_err = 0
         self.err_i = 0
+
+    def set_goal(self, goal):
+        self.goal = goal
 
     def getUpdate(self, current_val):
 
@@ -33,86 +36,6 @@ class PIDController:
 
         return update
     
-def init_node():
-    # Create drone node
-    rospy.init_node('pid_controller', anonymous=True)
-
-def takeoff():
-    takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=1)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        #Ensure that node has been subscribed to by takeoff
-        is_connected = takeoff_pub.get_num_connections() > 0
-        if is_connected:
-            takeoff_pub.publish()
-            rospy.loginfo('Took off')
-            break
-        rate.sleep()
-
-def land():
-    land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=1)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        connections = land_pub.get_num_connections()
-        if connections > 0:
-            land_pub.publish()
-            rospy.loginfo('Landed')
-            break
-        rate.sleep()
-
-def publish_velocity(vel_pub, linear_vel , angular_vel):
-    vel_msg = Twist()
-
-    vel_msg.linear.x = linear_vel[0]
-    vel_msg.linear.y = linear_vel[1]
-    vel_msg.linear.z = linear_vel[2]
-
-    vel_msg.angular.z = angular_vel
-
-    vel_pub.publish(vel_msg)
 
 
 
-def moveToTarget(goal):
-    vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    get_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-
-    linear_controller = PIDController(goal, 0.5, 0, 0)
-    angular_controller = PIDController(0, 0.5, 0, 0)
-
-    pos = get_model_state('quadrotor', "")
-    current_pos = np.array([pos.pose.position.x, pos.pose.position.y, pos.pose.position.z])
-
-    while (np.linalg.norm(current_pos - goal) >= 0.05):
-
-        # Get the current position and angular velocity
-        pos = get_model_state('quadrotor', "")
-        current_pos = np.array([pos.pose.position.x, pos.pose.position.y, pos.pose.position.z])
-        angular_z = pos.twist.angular.z
-
-        uLinear =  linear_controller.getUpdate(current_pos)
-        uAngular = angular_controller.getUpdate(angular_z)
-
-        publish_velocity(vel_pub, uLinear, uAngular)
-    
-    rospy.loginfo("Successfully arrived at location: %s ! ", current_pos)
-
-    stop_vel = np.zeros(3)
-    publish_velocity(vel_pub, stop_vel, 0)
-
-
-if __name__ == '__main__':
-    try:
-        try:
-            target = list(map(float, sys.argv[1].split(',')))
-            print(target)
-        except:
-            print("Please specify the target location in the form x,y,z")
-        init_node() # Create node
-        takeoff() # Takeoff
-        rospy.sleep(rospy.Duration(2)) # Allow time before moving
-        moveToTarget(np.array(target))
-        rospy.sleep(rospy.Duration(2))
-        land() # Land
-    except rospy.ROSInterruptException:
-        pass
